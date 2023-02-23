6fb06968c120a5012056f07ebb8056bc59a1d1c0
zhaomzheng <33871411+zhaomzheng@users.noreply.github.com>
2023-02-23 11:12:20 -0500

    Update menu-select.component.jsx

--- menu-select.component.jsx
+++ menu-select.component.jsx
@@ -1,39 +1,37 @@
-import { Fragment, useState } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Fragment, useState, useEffect } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const menus = [
-  { title: 'All Day Menu', start_time: '12:00PM', end_time: '12:00PM', current: true },
-  { title: 'Lunch Menu', start_time: '12:00PM', end_time: '12:00PM', current: false },
-  { title: 'Brunch Menu', start_time: '11:00AM', end_time: '2:00PM', current: false },
-  { title: 'Dinner Menu', start_time: '5:00PM', end_time: '10:00PM', current: false },
-];
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ')
 }
 
-export default function MenuSelect(props) {
-  
-  const [selected, setSelected] = useState(menus[0]);
+export default function MenuSelect({ menus, onChange, currentMenu }) {
+  const [selected, setSelected] = useState(menus[0])
+  useEffect(() => {
+    if (currentMenu) {
+      setSelected(currentMenu);
+    }
+  }, [currentMenu]);
 
-  function handleMenuChange(menu) {
-    setSelected(menu);
-    props.onChange(menu.title);
+  if (!selected) {
+    return null; // do not render Listbox if selected is not defined
   }
 
   return (
-    <Listbox value={selected} onChange={handleMenuChange}>
-      {({ open }) => (
+<Listbox value={currentMenu} onChange={onChange}>
+        {({ open }) => (
         <>
-          <Listbox.Label className="sr-only">Select a menu</Listbox.Label>
+          <Listbox.Label className="sr-only"> Change published status </Listbox.Label>
           <div className="relative">
-            <div className="inline-flex">
-              <div className="inline-flex">
-                <div className="inline-flex py-2 pr-1 text-black">
-                  <p className="text-sm font-poppins font-semibold">{selected.title}</p>
+            <div className="inline-flex ">
+              <div className="inline-flex ">
+                <div className="inline-flex  py-2  pr-1 text-black ">
+                  <p className="text-sm font-poppins font-semibold">{selected.menuName}</p>
+                  {console.log(selected.menuName)}
                 </div>
                 <Listbox.Button className="inline-flex items-center p-2 text-sm font-medium text-white h focus:outline-none  focus:ring-offset-2 focus:ring-offset-gray-50">
-                  <span className="sr-only">Change menu</span>
+                  <span className="sr-only">Change published status</span>
                   <ChevronDownIcon className="h-5 w-5 text-black" aria-hidden="true" />
                 </Listbox.Button>
               </div>
@@ -47,27 +45,40 @@ export default function MenuSelect(props) {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute left-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {menus.map((menu) => (
+                {menus.map((option) => (
                   <Listbox.Option
-                    key={menu.title}
-                    className={({ active }) =>
-                      (active ? 'text-black bg-gray-100' : 'text-gray-900') + ' cursor-default select-none p-4 text-sm font-poppins'
-                    }
-                    value={menu}
-                  >
-                    {({ selected, active }) => (
+                  key={option.menuName}
+                  className={({ active }) =>
+                    classNames(
+                      active ? 'text-black bg-gray-100' : 'text-gray-900',
+                      'cursor-default select-none p-4 text-sm font-poppins'
+                    )
+                  }
+                  value={option}
+                >
+                  {({ selected, active }) =>
+                    selected && active ? (
                       <div className="flex flex-col">
                         <div className="flex justify-between">
-                          <p className={selected ? 'font-medium text-menured-600' : 'font-medium'}>{menu.title}</p>
+                          <p className="font-medium text-menured-600">{option.menuName}</p>
                         </div>
-                        <p
-                          className={classNames(selected ? 'text-menured-600' : 'text-gray-900', 'mt-1 text-xs')}
-                        >
-                          {menu.start_time} - {menu.end_time}
+                        <p className="mt-1 text-xs text-menured-600">
+                          {option.startTime} - {option.endTime}
                         </p>
                       </div>
-                    )}
-                  </Listbox.Option>
+                    ) : (
+                      <div className="flex flex-col">
+                        <div className="flex justify-between">
+                          <p className="font-medium">{option.menuName}</p>
+                        </div>
+                        <p className="mt-1 text-xs text-gray-900">
+                          {option.startTime} - {option.endTime}
+                        </p>
+                      </div>
+                    )
+                  }
+                </Listbox.Option>
+                
                 ))}
               </Listbox.Options>
             </Transition>
@@ -75,5 +86,5 @@ export default function MenuSelect(props) {
         </>
       )}
     </Listbox>
-  );
+  )
 }
